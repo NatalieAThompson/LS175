1ecc619ebe082b141289d6184ae837baf24498e7
Natalie Thompson <Natalie.A.Thompson@outlook.com>
2021-07-16 20:38:04 -0500

    Made a server for a query param game

--- server.rb
+++ server.rb
@@ -1,20 +1,30 @@
 require "socket"
 
+def format_params(params)
+  return "/" if params.nil?
+  # rolls=2&sides=6
+  params = params[0].split("&")
+  # [rolls=2, sides=6]
+  params = params.map do |ele|
+    ele.split("=")
+  end.to_h
+end
+
 server = TCPServer.new("localhost", 3003)
 loop do
   client = server.accept
 
   request_line = client.gets
+  # GET /?rolls=2&sides=6 HTTP/1.1
   next if !request_line || request_line =~ /favicon/
   # puts request_line
 
-  http_method = "GET"
+  http_method, path_and_params, http = request_line.split
   # path = request_line.scan()
-  path = "/"
-  params = {
-    "rolls" => "2",
-    "sides" => "6"
-  }
+  # /?rolls=2&sides=6
+  path = path_and_params.scan(/(\/[a-z]*)/)[0]
+  params = path_and_params.scan(/\?(.+)/)[0]
+  params = format_params(params)
 
   client.puts "HTTP/1.1 200 OK"
   client.puts "Content-Type: text/plain\r\n\r\n"
