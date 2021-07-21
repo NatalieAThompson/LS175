eb04f3034110e4d482fa55f4d7df69bb3851aef1
Natalie Thompson <Natalie.A.Thompson@outlook.com>
2021-07-20 20:29:33 -0500

    Added HTML to my server ruby doc.

--- server.rb
+++ server.rb
@@ -15,24 +15,36 @@ loop do
   client = server.accept
 
   request_line = client.gets
-  # GET /?rolls=2&sides=6 HTTP/1.1
   next if !request_line || request_line =~ /favicon/
-  # puts request_line
 
   http_method, path_and_params, http = request_line.split
-  # path = request_line.scan()
-  # /?rolls=2&sides=6
+
   path = path_and_params.scan(/(\/[a-z]*)/)[0]
   params = path_and_params.scan(/\?(.+)/)[0]
   params = format_params(params)
 
-  client.puts "HTTP/1.1 200 OK"
-  client.puts "Content-Type: text/plain\r\n\r\n"
-  client.puts request_line
+  # client.puts "HTTP/1.1 200 OK"
+  # client.puts "Content-Type: text/plain\r\n\r\n"
+  # client.puts request_line
 
+  client.puts "HTTP/1.0 200 OK"
+  client.puts "Content-Type: text/html"
+  client.puts
+  client.puts "<html>"
+  client.puts "<body>"
+  client.puts "<pre>"
+  client.puts http_method
+  client.puts path
+  client.puts params
+  client.puts "</pre>"
 
-  params["rolls"].to_i.times do
-    client.puts(rand(params["sides"].to_i) + 1)
-  end
+  number = params["number"].to_i
+  client.puts "<p>The current number is #{number}.</p>"
+
+  client.puts "<a href='?number=#{number + 1}'>Add one</a>"
+  client.puts "<a href='?number=#{number - 1}'>Subtract one</a>"
+
+  client.puts "</body>"
+  client.puts "</html>"
   client.close
 end
